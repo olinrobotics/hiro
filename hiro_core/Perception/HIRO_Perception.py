3c39b6a67e4b35b2fe04d96f46289b07b37db5b0
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-04-12 17:27:06 -0400

    Refactor code

--- hiro_core/Perception/HIRO_Perception.py
+++ hiro_core/Perception/HIRO_Perception.py
@@ -1,43 +1,40 @@
-import numpy as np
-import random
+import matplotlib.pyplot as plt
+from cv_bridge import CvBridgeError, CvBridge
+
 import cv2
+import numpy as np
 import rospy
 import sensor_msgs.point_cloud2 as pc2
-from cv_bridge import CvBridgeError, CvBridge
 from sensor_msgs.msg import Image, PointCloud2
-from std_msgs.msg import Bool
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d, Axes3D
-import numpy as np
+
 
 class Perception:
     def __init__(self, depth, color, points):
-            self.bridge = CvBridge()
-            # In ROS, nodes are uniquely named. If two nodes with the same
-            # name are launched, the previous one is kicked off. The
-            # anonymous=True flag means that rospy will choose a unique
-            # name for our 'listener' node so that multiple listeners can
-            # run simultaneously.
-            rospy.init_node('listener', anonymous=True)
-            rospy.Subscriber(depth, Image, self.depth_callback, queue_size=10)
-            rospy.Subscriber(color, Image, self.color_callback, queue_size=10)
-            rospy.Subscriber(points, PointCloud2, self.pointcloud_callback, queue_size=10)
-            self.rgb_scaled_img = self.depth_scaled_img = self.point_cloud = None
+        self.bridge = CvBridge()
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # name are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber(depth, Image, self.depth_callback, queue_size=10)
+        rospy.Subscriber(color, Image, self.color_callback, queue_size=10)
+        rospy.Subscriber(points, PointCloud2, self.pointcloud_callback, queue_size=10)
+        self.rgb_scaled_img = self.depth_scaled_img = self.point_cloud = None
 
-
-    def color_callback(self,data):
+    def color_callback(self, data):
         try:
             self.rgb_scaled_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print e
 
-    def depth_callback(self,data):
+    def depth_callback(self, data):
         try:
-            self.depth_scaled_img  = self.bridge.imgmsg_to_cv2(data)
+            self.depth_scaled_img = self.bridge.imgmsg_to_cv2(data)
         except CvBridgeError as e:
-            print(e)
+            print e
 
-    def pointcloud_callback(self,data):
+    def pointcloud_callback(self, data):
         self.point_cloud = data
 
     def show_color(self):
@@ -46,7 +43,6 @@ class Perception:
             if self.rgb_scaled_img is None:
                 continue
 
-            c = cv2.applyColorMap(self.rgb_scaled_img.astype(np.uint8), cv2.COLORMAP_RAINBOW)
             cv2.imshow('Color Image', self.rgb_scaled_img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -77,18 +73,17 @@ class Perception:
         while self.point_cloud is None:
             print "No point cloud found"
 
-        self.gen = pc2.read_points(self.point_cloud, field_names=("x", "y", "z"))
+        gen = pc2.read_points(self.point_cloud, field_names=("x", "y", "z"))
 
         coords = []
-        count = 0
         for i, p in enumerate(gen):
             coords.append(p)
-            count += 1
-            if count > 100000:
+            if i > 100000:
                 break
 
         return np.asarray(coords)
 
+    @staticmethod
     def plot_cube3d(coords):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -97,26 +92,27 @@ class Perception:
         plt.show()
 
     def run(self):
-        #show color
+        # show color
         self.show_color()
 
-        #show depth
+        # show depth
         self.show_depth()
 
-        #collect pointcloud coords
+        # collect pointcloud coords
         self.get_pointcloud_coords()
 
-        #plot pointcloud coords
-        #self.plot_cube3d(coords)
+        # plot pointcloud coords
+        # Perception.plot_cube3d(coords)
 
         # spin() simply keeps python from exiting until this node is stopped
-        #rospy.spin()
+        # rospy.spin()
+
 
 if __name__ == '__main__':
-    #Change these to different rostopics according to the type of camera
+    # Change these to different rostopics according to the type of camera
     DepthSubscriber = '/kinect2/sd/image_depth_rect'
     ColorSubscriber = '/kinect2/hd/image_color'
     PointCloudSubscriber = '/kinect2/hd/points'
 
-    perception = Perception(DepthSubscriber,ColorSubscriber,PointCloudSubscriber)
+    perception = Perception(DepthSubscriber, ColorSubscriber, PointCloudSubscriber)
     perception.run()
