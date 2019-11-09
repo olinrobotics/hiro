32c7bcf38715fe06c09d0a038e2be600f4474091
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-11-08 20:11:17 -0500

    Add functionalities for GenericRobot to visualize a trajectory

--- hiro_core/GenericRobot.py
+++ hiro_core/GenericRobot.py
@@ -9,24 +9,22 @@ import sys
 import moveit_commander
 import rospy
 import geometry_msgs.msg
+import moveit_msgs.msg
+from math import pi
 
 
 class GenericRobot(object):
 
-    def __init__(self, node_name='generic_robot_move_group'):
+    def __init__(self, node_name='generic_robot_move_group', visualize_trajectory=True):
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node(node_name, anonymous=True)
+        self.visualize_trajectory = visualize_trajectory
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
+        self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                            moveit_msgs.msg.DisplayTrajectory,
+                                                            queue_size=20)
         rospy.sleep(0.2)
-
-        # Init ground pose
-        ground_pose = moveit_commander.PoseStamped()
-        ground_pose.header.frame_id = self.robot.get_planning_frame()
-        ground_pose.pose.position.x = 0
-        ground_pose.pose.position.y = 0
-        ground_pose.pose.position.z = 0
-        self.scene.add_plane("ground", ground_pose)
 
         # Find all available group names of the robot
         self.group_names = self.robot.get_group_names()
@@ -47,6 +45,51 @@ class GenericRobot(object):
             print "Cannot find group:", group_name
             return False
 
+    def publish_trajectory(self, plan):
+        # type: (moveit_msgs.msg.RobotTrajectory) -> None
+        """
+        Publish trajectory so that we can visualize in Rviz
+        :param plan:
+        :return:
+        """
+        display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+        display_trajectory.trajectory_start = self.get_robot_state()
+        display_trajectory.trajectory.append(plan)
+        self.display_trajectory_publisher.publish(display_trajectory)
+
+    def plan_and_execute(self, move_group, joint_goal=None):
+        # type: (moveit_commander.MoveGroupCommander, list) -> bool
+        """
+        Plan, display the trajectory and execute the plan
+        :param move_group: MoveGroupCommander
+        :param joint_goal: a list of joints
+        :return: bool: is_success
+        """
+        plan = move_group.plan(joint_goal)
+        while True:
+            if self.visualize_trajectory:
+                print "What do you want to do?" \
+                      "\n1. Re-visualize the plan (type 1)" \
+                      "\n2. Execute the plan(type execute)" \
+                      "\n3. Re-plan (type 3)" \
+                      "\n4. Do nothing (type 4)"
+                user_input = raw_input("Input:")
+                if user_input == "1":
+                    self.publish_trajectory(plan)
+                    continue
+                elif user_input == "execute":
+                    break
+                elif user_input == "3":
+                    plan = move_group.plan(joint_goal)
+                    continue
+                else:
+                    return False
+            else:
+                break
+
+        is_success = move_group.execute(plan, wait=True)
+        return is_success
+
     def set_pose_goal(self, group_name, pose_goal=None):
         # type: (str, geometry_msgs.msg.Pose) -> bool
         """
@@ -60,7 +103,7 @@ class GenericRobot(object):
             if self.contains(group_name):
                 move_group = self.move_groups[group_name]
                 move_group.set_pose_target(pose_goal)
-                is_success = move_group.go(wait=True)
+                is_success = self.plan_and_execute(move_group)
                 move_group.stop()
                 move_group.clear_pose_targets()
         else:
@@ -83,7 +126,7 @@ class GenericRobot(object):
                 print "Current joints are not the same as goal joints."
                 return is_success
 
-            is_success = self.move_groups[group_name].go(joint_goal, wait=True)
+            is_success = self.plan_and_execute(self.move_groups[group_name])
             self.move_groups[group_name].stop()
 
         return is_success
@@ -109,8 +152,6 @@ class GenericRobot(object):
 
 
 if __name__ == '__main__':
-    from math import pi
-
     robot = GenericRobot()
     move_group = "left_manipulator"
 
--- hiro_core/__init__.py
+++
@@ -1,1 +0,0 @@
-from hiro_core.hiro_core.include.hiro_core import *
