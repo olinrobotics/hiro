374573879a5844a2df96e071ee139fd2116d6dc3
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-11-28 00:04:25 -0500

    Add Game.py

---
+++ hiro_active/Connect4/Game.py
@@ -0,0 +1,136 @@
+from Simulation import Connect4Board
+
+
+class Game:
+    def __init__(self, rows=6, columns=7):
+        self._base_board = Connect4Board(rows=rows, columns=columns)
+
+    def getInitBoard(self):
+        """
+        Returns:
+            startBoard: a representation of the board (ideally this is the form
+                        that will be the input to your neural network)
+        """
+        return self._base_board.np_pieces
+
+    def getBoardSize(self):
+        """
+        Returns:
+            (x,y): a tuple of board dimensions
+        """
+        return self._base_board.rows, self._base_board.columns
+
+    def getActionSize(self):
+        """
+        Returns:
+            actionSize: number of all possible actions
+        """
+        return self._base_board.columns
+
+    def getNextState(self, board, player, action):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+            action: action taken by current player
+        Returns:
+            nextBoard: board after applying action
+            nextPlayer: player who plays in the next turn (should be -player)
+        """
+        cloned = self._base_board.clone(np_pieces=board)
+        cloned.make_move(action)
+        return cloned.np_pieces, -player
+
+    def getValidMoves(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player
+        Returns:
+            validMoves: a binary vector of length self.getActionSize(), 1 for
+                        moves that are valid from the current board and player,
+                        0 for invalid moves
+        """
+        return self._base_board.clone(np_pieces=board).get_valid_moves()
+
+    def getGameEnded(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+        Returns:
+            r: 0 if game has not ended. 1 if player won, -1 if player lost,
+               small non-zero value for draw.
+
+        """
+        b = self._base_board.with_np_pieces(np_pieces=board)
+        win_state = b.win_state
+        if win_state.terminated:
+            if win_state.winner is None:
+                return 1e-4
+            elif win_state.winner == player:
+                return 1
+            elif win_state.winner == -player:
+                return -1
+            else:
+                raise ValueError('Unexpected win state found: ', win_state)
+        else:
+            return 0
+
+    def getCanonicalForm(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+        Returns:
+            canonicalBoard: returns canonical form of board. The canonical form
+                            should be independent of player. For e.g. in chess,
+                            the canonical form can be chosen to be from the pov
+                            of white. When the player is white, we can return
+                            board as is. When the player is black, we can invert
+                            the colors and return the board.
+        """
+        return board * player
+
+    def getSymmetries(self, board, pi):
+        """
+        Input:
+            board: current board
+            pi: policy vector of size self.getActionSize()
+        Returns:
+            symmForms: a list of [(board,pi)] where each tuple is a symmetrical
+                       form of the board and the corresponding pi vector. This
+                       is used when training the neural network from examples.
+        """
+        # Board is left/right symmetric in Connect 4
+        return [(board, pi), (board[:, ::-1], pi[::-1])]
+
+    def stringRepresentation(self, board):
+        """
+        Input:
+            board: current boaee
+        Returns:
+            boardString: a quick conversion of board to a string format.
+                         Required by MCTS for hashing.
+        """
+        return board.toString()
+
+    @staticmethod
+    def display(board):
+        print(" -----------------------")
+        print(' '.join(map(str, range(len(board[0])))))
+        print(board)
+        print(" -----------------------")
+
+
+if __name__ == '__main__':
+    game = Game()
+    print(game.getInitBoard())
+    print(game.getBoardSize())
+    print(game.getActionSize())
+    print(game.getNextState(None, 1, 1))
+    print(game.getValidMoves())
+    print(game.getGameEnded(None, 1))
+    print(game.getCanonicalForm())
+    print(game.getSymmetries())
+    print(game.stringRepresentation())
--- hiro_active/Connect4/Simulation.py
+++ hiro_active/Connect4/Simulation.py
@@ -7,8 +7,13 @@ class WinState():
         self.winner = winner
 
 class Connect4Board():
-    def __init__(self, rows=6, columns=7):
-        self.board = np.full((rows, columns), 0)
+    def __init__(self, rows=6, columns=7, np_pieces=None):
+        self.rows = rows
+        self.columns = columns
+        if np_pieces is not None:
+            self.np_pieces = np_pieces
+        else:
+            self.np_pieces = np.full((rows, columns), 0)
         hor = np.array([[1, 1, 1, 1]])
         ver = np.transpose(hor)
         diag1 = np.eye(4, dtype=np.uint8)
@@ -20,9 +25,9 @@ class Connect4Board():
     def check_winning(self):
         # Boolean mask current player with 1
         copy = np.zeros((6, 7))
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                if self.board[i][j] == self.current_player:
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if self.np_pieces[i][j] == self.current_player:
                     copy[i][j] = 1
 
         # Check if current player is winning
@@ -33,8 +38,8 @@ class Connect4Board():
 
         # Check draw
         draw = True
-        for j in range(len(self.board[i])):
-            if self.board[0][j] == 0:
+        for j in range(self.columns):
+            if self.np_pieces[0][j] == 0:
                 draw = False
                 break
 
@@ -44,20 +49,27 @@ class Connect4Board():
             self.win_state = WinState(False, 0)
 
     def make_move(self, col):
-        invalid = True
-        for i in range(len(self.board) - 1, -1, -1):
-            if self.board[i][col] == 0:
-                self.board[i][col] = self.current_player
+        for i in range(self.rows - 1, -1, -1):
+            if self.np_pieces[i][col] == 0:
+                self.np_pieces[i][col] = self.current_player
                 self.check_winning()
-                self.switch_player()
-                invalid = False
-                break
+                if not self.win_state.terminated:
+                    self.switch_player()
+                return self.win_state
 
-        print("invalid move try again")
-        return invalid
+        raise ValueError("invalid move try again")
+
+    def get_valid_moves(self):
+        return self.np_pieces[0] == 0
 
     def switch_player(self):
         self.current_player = -self.current_player
+
+    def clone(self, np_pieces=None):
+        if np_pieces is None:
+            return Connect4Board(self.rows, self.columns, np_pieces=np.copy(self.np_pieces))
+        else:
+            return Connect4Board(self.rows, self.columns, np_pieces=np.copy(board))
 
     def game(self):
         while True:
@@ -66,8 +78,9 @@ class Connect4Board():
                 col = int(input(f'Team {self.current_player} choose column (1->7): ')) - 1
                 invalid = self.make_move(col)
 
-            print(self.board)
+            print(self.np_pieces)
             if self.win_state.terminated:
+                print(f'Player {self.current_player} won!')
                 break
 
 
