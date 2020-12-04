f9e430eb06744a0c4f763bb79fa30b93f81c8566
Audrey Lee <alee2@olin.edu>
2020-12-03 21:05:55 -0500

    Adding updated model

--- hiro_active/Connect4/model.py
+++ hiro_active/Connect4/model.py
@@ -4,51 +4,25 @@ import torch.nn.functional as fun
 import numpy as np
 import matplotlib
 
+class DQN(nn.Module):
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-
-    def forward(self, s):
-        s = s.view(-1, 3, 6, 7)  # compress into 1D vector, channels, board x, board y
-        s = self.conv1(s)
-        s = self.bn1(s)
-        s = fun.relu(s)
-        return s
-
-class RNN(nn.Module):
-    def __init__(self, input_size=32, output_size=32, stride=1, downsample=None):
-        super(RNN, self).__init__()
-        self.conv1 = nn.Conv2d(input_size, output_size, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(input_size)
+    def __init__(self, input_size, output_size, state_size, n_layers):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(input_size, state_size, n_layers)
+        self.bn1 = nn.BatchNorm2d(state_size)
+        self.head = nn.Linear(state_size, output_size)
 
     def forward(self, x):
-        res = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = fun.relu(out)
-        out += res
-        out = fun.relu(out)
-        return out
+        x = fun.relu(self.bn1(self.conv1(x)))
+        # compress into 1D vector
+        return self.head(x.view(x.size(0), -1))
 
-# Need a block to combine the output of the two nets
-    
-class Connect(nn.Module):
+class Loss(nn.Module):
     def __init__(self):
-        super(Connect, self).__init__()
-        self.conv = CNN()
-        blocks = 15
-        for block in range(blocks):
-            setattr(self, "res_%i" % block,RNN())
-        # Put connected output block here self.output
-    
-    def forward(self,s):
-        s = self.conv(s)
-        for block in range(blocks):
-            s = getattr(self, "res_%i" % block)(s)
-        # Put connected output block here s =
-        return s
+        super(Loss, self).__init__()
 
-# Include loss
+    def forward(self, val, val2, policy, policy2):
+        val_err = (val2 - val) ** 2
+        policy_err = torch.sum((-policy2*torch.log(1e-8 + policy)), 1)
+        total_err = torch.mean((val_err.view(-1) + policy_err))
+        return total_err
