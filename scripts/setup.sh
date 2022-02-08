8a3b9012a937708795891f3cd09978451a165f7a
Krishna Suresh <krishna4132@gmail.com>
2022-02-08 15:04:34 -0500

    third party package changing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,10 @@
 # install 
 source /opt/ros/<your_ros_version>/setup.bash
+rm -rf src/third-party/
 git clone -b boost https://github.com/UniversalRobots/Universal_Robots_Client_Library.git src/third-party/Universal_Robots_Client_Library
-git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git src/third-party/Universal_Robots_ROS_Driver
-git clone -b calibration_devel https://github.com/fmauch/universal_robot.git src/third-party/fmauch_universal_robot
-git clone https://github.com/Danfoa/robotiq_2finger_grippers src/third-party/robotiq_2finger_grippers
+git clone https://github.com/olinrobotics/Universal_Robots_ROS_Driver.git src/third-party/Universal_Robots_ROS_Driver
+git clone https://github.com/olinrobotics/universal_robot.git src/third-party/universal_robot
+git clone https://github.com/olinrobotics/robotiq_2finger_grippers.git src/third-party/robotiq_2finger_grippers
 sudo apt update -qq
 rosdep update
 rosdep install --from-paths src --ignore-src -y
